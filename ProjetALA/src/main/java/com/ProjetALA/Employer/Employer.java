12d6173e6eee847f9c943484ce48f56837f9c83e
INTI0250 <INTI0250@DESKTOP-4IFI8J5>
2016-05-03 15:20:24 +0200

    Arnaud - correction employer

---
+++ ProjetALA/src/main/java/com/ProjetALA/Employer/Employer.java
@@ -0,0 +1,56 @@
+
+package com.ProjetALA.Employer;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.ProjetALA.Personne.Personne;
+import com.ProjetALA.Reservation.Reservation;
+
+/*Auteur:Rengassamy Alex
+ * nom Projet :ProjetALA
+ *date:02/05/2016
+ *Package:com.ProjetALA
+ *Class:Employer
+ *association:List<Reservation>
+ * 
+ * */
+@Entity
+@DiscriminatorValue("Employer")
+public class Employer extends Personne {
+
+	/************* Association ***********/
+	
+	@OneToMany(mappedBy = "employe")
+	private List<Reservation> tabReservation = new ArrayList<Reservation>();
+	
+
+	/******** getterETsetter *********************/
+
+	public List<Reservation> getTabReservation() {
+		return tabReservation;
+	}
+
+	public void setTabReservation(List<Reservation> tabReservation) {
+		this.tabReservation = tabReservation;
+	}
+
+	/************** Constructor issu de la classe mère *********************/
+	public Employer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employer(String nomPersonne, String prenom, String sex,
+			Date dateNaissance, String adresse, String tel, String mail) {
+		super(nomPersonne, prenom, sex, dateNaissance, adresse, tel, mail);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
